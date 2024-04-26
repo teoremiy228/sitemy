7d98f83d8db60d171942cff06e0f09907d051d8a
Artemiy Yastremskiy <cleversolimp@gmail.com>
2024-04-26 14:02:29 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,45 @@
-let counter = 0;
-function Hermann(){
-    counter++;
-    let h1 = document.getElementById("bigtext")
-    if(counter % 2 == 1){
-        h1.innerHTML = "Hermann ne otvlekaisia"
+let currentState = 0;
+const scenarios = [
+    {
+        text: "Вы просыпаетесь в темной комнате. Куда пойдете?",
+        choices: [
+            "Открыть дверь",
+            "Остаться на месте"
+        ],
+        next: [1, 2]
+    },
+    {
+        text: "За дверью был светлый коридор и выход на улицу. Вы свободны!",
+        choices: [
+            "Начать заново",
+            ""
+        ],
+        next: [0, 0]
+    },
+    {
+        text: "Вы решили остаться. В комнате становится всё темнее...",
+        choices: [
+            "Начать заново",
+            ""
+        ],
+        next: [0, 0]
     }
-    else{
-        h1.innerHTML = "Miras ne otvlekaisia"
-    }
+];
+
+// Функция для вывода текущего состояния игры
+function displayState() {
+    const scenario = scenarios[currentState];
+    document.getElementById('story').innerText = scenario.text;
+    document.getElementById('choice1').innerText = scenario.choices[0];
+    document.getElementById('choice2').innerText = scenario.choices[1];
+
+    // Скрываем вторую кнопку, если второй выбор пуст
+    document.getElementById('choice2').style.display = scenario.choices[1] ? 'block' : 'none';
 }
-
- function Anuar(){
-    let nameElement = document.getElementById("inputName");
-    let name = nameElement.value;
-
-    let mailElement = document.getElementById("inputMail");
-    let mail = mailElement.value;
-
-   
-    console.log(name, mail)
+function makeChoice(choice) {
+    currentState = scenarios[currentState].next[choice - 1];
+    displayState();
 }
+window.onload = function() {
+    displayState();
+};
