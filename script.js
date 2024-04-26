0d126f80119d5796cda31597cae22afa6487cf09
Artemiy Yastremskiy <cleversolimp@gmail.com>
2024-04-26 14:02:47 +0600

    Update script.js

--- script.js
+++ script.js
@@ -26,14 +26,12 @@ const scenarios = [
     }
 ];
 
-// Функция для вывода текущего состояния игры
 function displayState() {
     const scenario = scenarios[currentState];
     document.getElementById('story').innerText = scenario.text;
     document.getElementById('choice1').innerText = scenario.choices[0];
     document.getElementById('choice2').innerText = scenario.choices[1];
 
-    // Скрываем вторую кнопку, если второй выбор пуст
     document.getElementById('choice2').style.display = scenario.choices[1] ? 'block' : 'none';
 }
 function makeChoice(choice) {
